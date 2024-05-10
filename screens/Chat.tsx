bd2059ee04f223a7713145b777d56e185339f53a
zhouguangsi <zhouguangsi@pintechs.com>
2024-05-10 14:10:16 +0800

    touch项目初始化

---
+++ screens/Chat.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  View,
+  FlatList,
+} from "react-native";
+import { Icon, Message } from "../components";
+import DEMO from "../assets/data/demo";
+import styles, { DARK_GRAY } from "../assets/styles";
+import { LinearGradient } from 'expo-linear-gradient'
+
+const Messages = () => (
+  <LinearGradient
+    colors={['#F4FDFF', '#FFFEF8']}
+    style={styles.bg}
+  >
+    <View style={styles.containerMessages}>
+      <View style={styles.top}>
+        <Text style={styles.title}>Chat</Text>
+        <TouchableOpacity>
+          <Icon name="ellipsis-vertical" color={DARK_GRAY} size={20} />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={DEMO}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Message
+              image={item.image}
+              name={item.name}
+              lastMessage={item.message}
+            />
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  </LinearGradient>
+);
+
+export default Messages;
---
+++ screens/Home.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { View, ImageBackground } from "react-native";
+import CardStack, { Card } from "react-native-card-stack-swiper";
+import { City, Filters, CardItem } from "../components";
+import styles from "../assets/styles";
+import DEMO from "../assets/data/demo";
+import { LinearGradient } from 'expo-linear-gradient'
+
+const Home = () => {
+  const [swiper, setSwiper] = useState<CardStack | null>(null);
+
+  return (
+    <LinearGradient
+      colors={['#F4FDFF', '#FFFEF8']}
+      style={styles.bg}
+    >
+      <View style={styles.containerHome}>
+        <View style={styles.top}>
+          <City />
+          <Filters />
+        </View>
+
+        <CardStack
+          loop
+          verticalSwipe={false}
+          renderNoMoreCards={() => null}
+          ref={(newSwiper): void => setSwiper(newSwiper)}
+        >
+          {DEMO.map((item) => (
+            <Card key={item.id}>
+              <CardItem
+                hasActions
+                image={item.image}
+                name={item.name}
+                description={item.description}
+                matches={item.match}
+              />
+            </Card>
+          ))}
+        </CardStack>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default Home;
